dc6bc73ce0618bf7698f56b40f49a0d7ff2984cd
Aayush Basnet <aayush9823@gmail.com>
2026-07-12 20:21:36 +0545

    Task 7 — App.jsx

---
+++ portfolio-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/layouts/Navbar";
+import HomePage from "./Pages/Home.jsx";
+import Footer from "./components/layouts/Footer.jsx"
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio-frontend/src/Pages/Home.jsx
@@ -0,0 +1,28 @@
+import Hero from "../components/sections/Hero.jsx"
+const HomePage = () => {
+    return (
+        <>
+            <section >
+                <Hero />
+            </section>
+
+            <section id="about">
+                <h1></h1>
+            </section>
+
+            <section id="skills">
+                <h1></h1>
+            </section>
+
+            <section id="projects">
+                <h1></h1>
+            </section>
+
+            <section id="contact">
+                <h1></h1>
+            </section>
+        </>
+    );
+};
+
+export default HomePage;
